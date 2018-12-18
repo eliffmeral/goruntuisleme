7147471b5433a4ae5ec036ee041600db48db1b68
eliffmeral <43914340+eliffmeral@users.noreply.github.com>
2018-12-18 21:02:20 +0300

    gonderme

---
+++ yineyenidennesnetakip/yineyenidennesnetakip/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace yineyenidennesnetakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.webcamBox = new System.Windows.Forms.ComboBox();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(54, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(346, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(257, 145);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // webcamBox
+            // 
+            this.webcamBox.FormattingEnabled = true;
+            this.webcamBox.Location = new System.Drawing.Point(54, 173);
+            this.webcamBox.Name = "webcamBox";
+            this.webcamBox.Size = new System.Drawing.Size(121, 21);
+            this.webcamBox.TabIndex = 2;
+            // 
+            // portBox
+            // 
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(54, 271);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(121, 21);
+            this.portBox.TabIndex = 3;
+            this.portBox.SelectedIndexChanged += new System.EventHandler(this.portBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Bağlan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(346, 189);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Nesne Yakala";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(337, 271);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(474, 271);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(623, 271);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM5";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 343);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(813, 25);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 368);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.webcamBox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox webcamBox;
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ yineyenidennesnetakip/yineyenidennesnetakip/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+
+namespace yineyenidennesnetakip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac;
+        int R, G, B;
+        int X, Y;
+        Graphics g;
+        int mode;
+        Bitmap imge1,imge2;
+
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            portBox.DataSource = SerialPort.GetPortNames();
+            int sayi = portBox.Items.Count;
+            if (sayi == 0)
+            {
+                toolStripLabel1.Text = "Aygıt bulunamadı. Bağlantıyı kontrol et!!";
+                portBox.Enabled = false;
+            }
+            else if (sayi > 0)
+            {
+                toolStripLabel1.Text = sayi + "tane port var";
+            }
+
+            string[] Portlar = SerialPort.GetPortNames();
+            foreach (string port in Portlar)
+            
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                webcamBox.Items.Add(Device.Name);
+
+            }
+
+            webcamBox.SelectedIndex = 0;
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalFrame.IsRunning == true)
+            {
+                FinalFrame.Stop();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
+            {
+                mode = 1;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = 9600;
+            if (serialPort1.IsOpen)
+            {
+                toolStrip1.Text = portBox.SelectedItem.ToString() + "Portuna bağlandı";
+            }
+        }
+        private void portBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = portBox.SelectedItem.ToString();
+            serialPort1.Open();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[webcamBox.SelectedIndex].MonikerString);
+
+            FinalFrame.NewFrame += FinalFrame_NewFrame;
+            FinalFrame.Start();
+        }
+
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            imge1 = (Bitmap)eventArgs.Frame.Clone();  
+            Bitmap imge2 = (Bitmap)eventArgs.Frame.Clone();
+
+            if (radioButton1.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(225, 0, 0));
+                filter.Radius = 100;
+                filter.ApplyInPlace(imge2);
+            }
+            if (radioButton2.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+
+                filter.CenterColor = new RGB(Color.FromArgb(0, 255, 0));
+                filter.Radius = 100;
+                filter.ApplyInPlace(imge2);
+
+            }
+            if (radioButton3.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+
+                filter.CenterColor = new RGB(Color.FromArgb(0, 0, 255));
+                filter.Radius = 100;
+                filter.ApplyInPlace(imge2);
+            }
+            switch (mode)
+            {
+                case 2:
+                    {
+                        g = Graphics.FromImage(imge2);
+                        g.DrawString(sayac.ToString(), new Font("Arial", 100), new SolidBrush(Color.Black), new PointF(2, 2));
+                        g.Dispose();
+
+                    }
+                    break;
+
+                case 1:
+                    {
+                        BlobCounter blob = new BlobCounter();
+                        blob.MinHeight = 15;
+                        blob.MinWidth = 15;
+                        blob.ObjectsOrder = ObjectsOrder.Size;
+                        blob.ProcessImage(imge2);
+                        Rectangle[] rects = blob.GetObjectsRectangles();
+                        if (rects.Length > 0)
+                        {
+                            Rectangle obje = rects[0];
+                            Graphics g = Graphics.FromImage(imge2);
+                            using (Pen pen = new Pen(Color.Black, 3))
+                            {
+                                g.DrawRectangle(pen, obje);
+                            }
+                            X = obje.X;
+                            Y = obje.Y;
+
+                            g = Graphics.FromImage(imge2);
+
+                            string alan = "";
+
+                            if (X >= 0 && 210 > X && Y >= 0 && Y < 160)
+                            {
+                                alan = "1.Bölge";
+                                serialPort1.Write("1");
+                            }
+                            else if (X >= 210 && X < 420 && Y >= 0 && Y < 160)
+                            {
+                                alan = "2.Bölge";
+                                serialPort1.Write("2");
+                            }
+                            else if (X >= 420 && X < 640 && Y >= 0 && Y < 160)
+                            {
+                                alan = "3.Bölge";
+                                serialPort1.Write("3");
+                            }
+
+                            else if (X >= 0 && X < 210 && Y >= 160 && 320 >= Y)
+                            {
+
+                                alan = "4.Bölge";
+
+                                serialPort1.Write("4");
+                            }
+                            else if (X >= 210 && X < 420 && Y >= 160 && 320 >= Y)
+                            {
+
+                                alan = "5.Bölge";
+
+                                serialPort1.Write("5");
+                            }
+                            else if (X >= 420 && 640 >= X && Y >= 160 && 320 >= Y)
+                            {
+
+                                alan = "6.Bölge";
+
+                                serialPort1.Write("6");
+
+                            }
+                            else if (X >= 0 && 210 >= X && Y >= 320 && 480 >= Y)
+                            {
+
+                                alan = "7.Bölge";
+
+                                serialPort1.Write("7");
+
+                            }
+                            else if (X >= 210 && 420 >= X && Y >= 320 && 480 >= Y)
+                            {
+
+                                alan = "8.Bölge";
+
+                                serialPort1.Write("8");
+
+                            }
+                            else if (X >= 420 && 640 >= X && Y >= 320 && 480 >= Y)
+                            {
+
+                                alan = "9.Bölge";
+
+                                serialPort1.Write("9");
+
+                            }
+                            g.DrawString(X.ToString() + "X" + Y.ToString() + alan, new Font("Arial", 12), Brushes.Red, new System.Drawing.Point(1, 1));
+                            g.Dispose();
+                        }
+                        pictureBox2.Image = imge2;
+
+                    }
+                    break;
+            }
+
+            pictureBox1.Image = imge1;
+
+        }
+    }
+}
